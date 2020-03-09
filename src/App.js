0a076b9bf565ea055ef7ee4871c75a66d2587a19
developer485 <asram517@gmail.com>
2020-03-09 08:08:09 +0530

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homePage.component';
 
-function App() {
+const HatsPage = () => {
     return (
         <div>
-            <HomePage />
+            <h1>Hats Page</h1>
         </div>
     )
 }
+
+const App = () => {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage}></Route>
+                <Route path='/hats' component={HatsPage}></Route>
+            </Switch>
+        </div>
+    )
+}
+
+// const HomePage = (props) => {
+//     console.log(props)
+//     return (
+//         <div>
+//             <Link to="/topics">Topics</Link>
+//             <h1>Home Page</h1>
+//         </div>
+//     );
+// };
+
+// const TopicList = (props) => {
+//     console.log(props);
+//     return (
+//         <div>
+//             <h1>Topic List</h1>
+//             <Link to={`${props.match.url}/13`}>To Topic 13</Link>
+//             <Link to={`${props.match.url}/17`}>To Topic 17</Link>
+//             <Link to={`${props.match.url}/21`}>To Topic 21</Link>
+//         </div>
+//     );
+// };
+
+// const TopicDetail = (props) => {
+//     console.log(props);
+//     return (
+//         <div>
+//             <h1>Topic Details: {props.match.params.topicId}</h1>
+//         </div>
+//     );
+// };
+
+// function App() {
+//     return (
+//         <div>
+//             <BrowserRouter>
+//                 <Route extact path='/' component={HomePage} />
+//                 <Route extact path="/topics" component={TopicList} />
+//                 <Route path="/topics/:topicId" component={TopicDetail} />
+//             </BrowserRouter>
+//         </div>
+//     )
+// }
 
 export default App;
